4114bba5626999114f496486fd75b29913fa12ac
Shakti.Misra <shakti.misra.works@gmail.com>
2022-03-13 21:55:22 +0530

    Adding dummmy data

--- db/entities/SkillMatrics.cds
+++ db/entities/SkillMatrics.cds
@@ -15,8 +15,9 @@ using {
 entity SkillMatrics : cuid, managed {
     user        : Association to one Users @mandatory;
     user_skills : Composition of many {
-                      skill       : Association to one Skills @mandatory;
-                      skill_level : SkillLevel                @mandatory;
-                      comments    : String(1000) default null;
+                      key ID          : UUID;
+                          skill       : Association to one Skills @mandatory;
+                          skill_level : SkillLevel                @mandatory;
+                          comments    : String(1000) default null;
                   }
 }
--- srv/src/main/java/smsap/ssm/handlers/CatalogServiceHandler.java
+++ srv/src/main/java/smsap/ssm/handlers/CatalogServiceHandler.java
@@ -7,20 +7,32 @@ import org.springframework.stereotype.Component;
 import com.sap.cds.services.cds.CdsService;
 import com.sap.cds.services.handler.EventHandler;
 import com.sap.cds.services.handler.annotations.After;
+import com.sap.cds.services.handler.annotations.On;
 import com.sap.cds.services.handler.annotations.ServiceName;
 
 import cds.gen.catalogservice.CatalogService_;
+import lombok.extern.slf4j.Slf4j;
 import cds.gen.catalogservice.Books;
 
 @Component
 @ServiceName(CatalogService_.CDS_NAME)
+@Slf4j
 public class CatalogServiceHandler implements EventHandler {
 
 	@After(event = CdsService.EVENT_READ)
 	public void discountBooks(Stream<Books> books) {
+		String methodName = "discountBooks";
+		log.info("CatalogServiceHandler::{} Start", methodName);
 		books.filter(b -> b.getTitle() != null && b.getStock() != null)
-		.filter(b -> b.getStock() > 200)
-		.forEach(b -> b.setTitle(b.getTitle() + " (discounted)"));
+				.filter(b -> b.getStock() > 200)
+				.forEach(b -> b.setTitle(b.getTitle() + " (discounted)"));
+		log.info("CatalogServiceHandler::{} End", methodName);
 	}
 
+	@On(event = CdsService.EVENT_READ)
+	public void checkBooks(Stream<Books> books) {
+		String methodName = "checkBooks";
+		log.info("CatalogServiceHandler::{} Start", methodName);
+		log.info("CatalogServiceHandler::{} End", methodName);
+	}
 }
